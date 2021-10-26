ed53b6875f3e69a01b30684a790d57bc488cbb84
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2021-10-26 17:52:57 +0200

    Fix wrong export

--- packages/bucketizers/index.ts
+++ packages/bucketizers/index.ts
@@ -1,3 +1,3 @@
 export * from '@treecg/substring-bucketizer';
 export * from '@treecg/basic-bucketizer';
-export * from '@treecg/substring-bucketizer';
+export * from '@treecg/subject-page-bucketizer';
